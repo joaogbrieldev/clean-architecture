39afc9ff177f4be23fa504fbbf855a43ae02af41
João Figueiredo <joao.ggwp@outlook.com>
2024-10-31 20:39:11 -0300

    test: create new unit test
      create test to `find customer` separate to use case test

--- src/data-layer/use-cases/customer/find-customer/find-customer.unit.spec.ts
+++ src/data-layer/use-cases/customer/find-customer/find-customer.unit.spec.ts
@@ -1,22 +1,30 @@
 import Customer from "../../../../domain/customer/entity/customer";
-import CustomerRepository from "../../../../infra/customer/repository/sequelize/customer.repository";
 import Address from "../../../../value-object/address";
 import { IInputFindCustomerDto, IOutputFindCustomerDto } from "./find-customer.dto";
 import { FindCustomerUseCase } from "./find-customer.use-case";
 
+const customerCreate = new Customer('any','any')
+const address: Address = new Address(
+  'any',
+  1,
+  'any',
+  'any',
+    )
+customerCreate.changeAddress(address)
+
+const mockRepository = () => {
+  return {
+    find: jest.fn().mockReturnValue(customerCreate),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn()
+  }
+}
+
 describe("Unit test", () => {
   test("should be find a customer with use case", async() => {
-    const customerRepository = new CustomerRepository();
+    const customerRepository = mockRepository();
     const usecase = new FindCustomerUseCase(customerRepository)
-    const customerCreate = new Customer('any','any')
-    const address: Address = new Address(
-      'any',
-       1,
-       'any',
-       'any',
-    )
-    customerCreate.changeAddress(address)
-    const customerCreated = await customerRepository.create(customerCreate);
 
     const input: IInputFindCustomerDto = {
       id: 'any'
